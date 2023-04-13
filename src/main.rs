0cc403f070c10bfe1aead54de84e580d22e14442
Zynh0722 <Zynh0722@gmail.com>
2023-04-13 04:20:01 -0700

    loading env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let league_folder = env::var("LEAGUE_FOLDER").unwrap();
+
+    println!("{}", league_folder);
 }
